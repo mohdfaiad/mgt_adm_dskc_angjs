71463910d0b01789b254dbd89adcbefda98aef37
Elton Bahia <eltonbahiabc@gmail.com>
2017-04-21 15:18:12 +0200

    #refatoracao

--- app/common/components/valueBox.js
+++ app/common/components/valueBox.js
@@ -10,7 +10,7 @@ angular.module('managementAdminCloud').component('valueBox', {
     controller: [
         'gridSystem',
         function (gridSystem) {
-            this.gridClasses = gridSystem.toCssClasses(this.grid)
+            this.$onInit = () => this.gridClasses = gridSystem.toCssClasses(this.grid)
         }
     ],
 
